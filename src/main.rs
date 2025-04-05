fc60d851f27ce161174888c2023ad87a99d82000
SanjayKumar <0508sanjaykumar@gmail.com>
2025-04-05 13:20:24 +0530

    Added environment and camera

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use bevy::prelude::*;
 
+fn spawn_ball(mut commands: Commands) {
+    println!("Spawning ball...");
+    commands.spawn_empty();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands
+      .spawn(Camera2d);
+}
+  
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .run();
+App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, (spawn_ball, spawn_camera))
+    .run();
 }
